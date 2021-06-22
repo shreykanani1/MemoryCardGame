69a646c79a9130f9cbfa6f777954025aecd52417
shreykanani1 <81901823+shreykanani1@users.noreply.github.com>
2021-06-22 13:27:36 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -5,22 +5,73 @@ let isSecondTurn = false;
 let firstCard;
 let secondCard;
 
-function checkCard() {
-//   console.log(this);
-  this.classList.add("flip");
+let imgArr = [
+  "android.png",
+  "c.png",
+  "cpp.png",
+  "csharp.png",
+  "css.png",
+  "fb.png",
+  "google.png",
+  "html.png",
+  "java.png",
+  "js.png",
+  "net.png",
+  "php.png",
+  "python.jfif",
+  "twitter.png",
+  "youtube.png",
+  "android.png",
+  "c.png",
+  "cpp.png",
+  "csharp.png",
+  "css.png",
+  "fb.png",
+  "google.png",
+  "html.png",
+  "java.png",
+  "js.png",
+  "net.png",
+  "php.png",
+  "python.jfif",
+  "twitter.png",
+  "youtube.png",
+];
 
+(function (array, a) {
+  for (var i = array.length - 1; i > 0; i--) {
+    var j = Math.floor(Math.random() * (i + 1));
+    var temp = array[i];
+    array[i] = array[j];
+    array[j] = temp;
+  }
+  for (let index = 0; index < imgArr.length; index++) {
+    let oldClass = a[index].classList[1];
+    let newClass = imgArr[index].split(".")[0];
+
+    let imgTag = a[index].firstElementChild;
+    imgTag.src = imgArr[index];
+
+    a[index].classList.remove(oldClass);
+    a[index].classList.add(newClass);
+    console.log(a[index].classList[1] + " " + newClass);
+  }
+})(imgArr, a);
+
+function checkCard() {
+  this.classList.add("flip");
+  console.log(this);
   if (!isSecondTurn) {
     isSecondTurn = true;
     firstCard = this;
-  } 
-  else {
+  } else {
     isSecondTurn = false;
     secondCard = this;
 
     let type1 = String(firstCard.classList[1]);
     let type2 = String(secondCard.classList[1]);
-    // console.log(type1);
-    // console.log(type2);
+    console.log(type1);
+    console.log(type2);
 
     if (type1 !== type2) {
       setTimeout(() => {
@@ -34,3 +85,11 @@ function checkCard() {
 a.forEach((element) => {
   element.addEventListener("click", checkCard);
 });
+
+window.sessionStorage.setItem("username", "shrey");
+window.sessionStorage.setItem("email", "shrey@gmail.com");
+console.log(sessionStorage);
+console.log(sessionStorage.getItem("username"));
+console.log(sessionStorage.key(0));
+console.log(sessionStorage.removeItem("username"));
+console.log(sessionStorage.clear());
